5d8e79475577b0d00edb1b32ea4d601a9ab28ae6
dymoy <148160802+dymoy@users.noreply.github.com>
2024-01-20 16:03:31 -0800

    Successfully implemented POST /api/notes route. Required npm package 'generate-unique-id' to give each note a unique ID.

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // Import npm dependencies 
 const express = require('express');
 const path = require('path');
-const fs = require('fs');
 
 // Instantiate Express app and PORT to listen to 
 const app = express();
@@ -10,13 +9,7 @@ const PORT = process.env.PORT || 3001;
 // Require Routes
 require('./routes/routes')(app);
 
-// Middleware for parsing application/json
-app.use(express.json());
-
-// Middleware for parsing urlencoded data 
-app.use(express.urlencoded({ extended: true }));
-
-// // Add a static middleware for serving assets in the public folder
+// Add a static middleware for serving assets in the public folder
 app.use(express.static('public'));
 
 app.listen(PORT, () =>
